89aab1cd2308cba1ab9bc493f5330ce03731c48a
“Lesley <you@example.com>
2023-12-15 20:45:28 -0500

    day 12 part 2 finally solved

--- aoc/day12/part2.js
+++ aoc/day12/part2.js
@@ -22,50 +22,58 @@ const exampleData = example.split("\n")
                             }
                             ))
 
-// const countWays = (config, numbers) => {
+const cache = new Map();
 
-//     if (config === '') {
-//         if (numbers.length === 0) {
-//             return 1;
-//         }
-//         return 0;
-//     }
+const countWays = (config, numbers) => {
 
-//     if (numbers.length === 0) {
-//         if (config.includes('#')) {
-//             return 0;
-//         }
-//         return 1;
-//     }
+    if (config === '') {
+        if (numbers.length === 0) {
+            return 1;
+        }
+        return 0;
+    }
 
-//     let result = 0;
+    if (numbers.length === 0) {
+        if (config.includes('#')) {
+            return 0;
+        }
+        return 1;
+    }
 
-//     if (config[0] === '.' || config[0] === '?') {
-//         result += countWays(config.substring(1), numbers);
-//     }
+    const key = `${config}-${numbers}`;
+    if (cache.has(key)) {
+        return cache.get(key);
+    }
 
-//     if (config[0] === '#' || config[0] === '?') {
-//         if (
-//             numbers[0] <= config.length &&
-//             !config.substring(0, numbers[0]).includes('.') &&
-//             (numbers[0] === config.length || config[numbers[0]] !== '#')
-//         ) {
-//             if (numbers[0] === config.length) {
-//                 result += countWays('', numbers.slice(1));
-//             } else {
-//                 result += countWays(config.substring(numbers[0] + 1), numbers.slice(1));
-//             }
-//         }
-//     }
-//     return result
-// }
+    let result = 0;
 
-// const findTotal = (input) => {
-//     let total = 0
-//     input.forEach(pair => {
-//         total += countWays(pair[0], pair[1])
-//     })
-// return total 
-// }
+    if (config[0] === '.' || config[0] === '?') {
+        result += countWays(config.substring(1), numbers);
+    }
 
-// console.log(findTotal(data))
+    if (config[0] === '#' || config[0] === '?') {
+        if (
+            numbers[0] <= config.length &&
+            !config.substring(0, numbers[0]).includes('.') &&
+            (numbers[0] === config.length || config[numbers[0]] !== '#')
+        ) {
+            if (numbers[0] === config.length) {
+                result += countWays('', numbers.slice(1));
+            } else {
+                result += countWays(config.substring(numbers[0] + 1), numbers.slice(1));
+            }
+        }
+    }
+    cache.set(key, result);
+    return result
+}
+
+const findTotal = (input) => {
+    let total = 0
+    input.forEach(pair => {
+        total += countWays(pair[0], pair[1])
+    })
+return total 
+}
+
+console.log(findTotal(data))
