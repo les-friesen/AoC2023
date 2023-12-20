4238ad085cd1fd317ed3a370c44c04cb193e11a6
“Lesley <you@example.com>
2023-12-19 23:51:08 -0500

    day 19 solution

---
+++ aoc/day19/part1.js
@@ -0,0 +1,95 @@
+const { inputData, example } = require("./data.js");
+
+const parseData = (data) => {
+    let [workflows, parts] = data.split("\n\n")
+    parts = parts.replace(/[xmas=]/g, "").replaceAll("{", "[").replaceAll("}", "]").split("\n").map(x => JSON.parse(x))
+    workflows = Object.fromEntries(workflows.replace(/[}]/g, "").split("\n").map(x => x.split("{").map(y => y.split(","))))
+    return [workflows, parts]
+}
+
+const part1 = (data) => {
+    let accepted = []
+    const [workflows, parts] = parseData(data);
+    parts.forEach(part => {
+        let current = "in"
+        while (current !== "A" && current !== "R") {
+            for (let i = 0; i < workflows[current].length; i++) {
+            if (workflows[current][i] === "A") {
+                current = "A"
+                break;
+            } else if (workflows[current][i] === "R") {
+                current = "R"
+                break;
+            } else if (Object.keys(workflows).includes(workflows[current][i])) {
+                current = workflows[current][i]
+                break;
+            } else {
+                const [, arg, num, dest] = workflows[current][i].match(/([xmas><]+)([\d]+):([a-zA-Z]+)/)
+                    if (arg === "x<") {
+                        if (part[0] < +num) {
+                            current = dest
+                            break;
+                        } else {
+                            continue
+                        }
+                    } else if (arg === "x>") {
+                        if (part[0] > +num) {
+                            current = dest
+                            break;
+                        } else {
+                            continue
+                        }
+                    } else if (arg === "m<") {
+                        if (part[1] < +num) {
+                            current = dest
+                            break;
+                        } else {
+                            continue
+                        }
+                    } else if (arg === "m>") {
+                        if (part[1] > +num) {
+                            current = dest
+                            break;
+                        } else {
+                            continue
+                        }
+                    } else if (arg === "a<") {
+                        if (part[2] < +num) {
+                            current = dest
+                            break;
+                        } else {
+                            continue
+                        }
+                    } else if (arg === "a>") {
+                        if (part[2] > +num) {
+                            current = dest
+                            break;
+                        } else {
+                            continue
+                        }
+                    } else if (arg === "s<") {
+                        if (part[3] < +num) {
+                            current = dest
+                            break;
+                        } else {
+                            continue
+                        }
+                    } else if (arg === "s>") {
+                        if (part[3] > +num) {
+                            current = dest
+                            break;
+                        } else {
+                            continue
+                        }
+                    }
+                }
+            }
+        }
+        if (current === "A") {
+            accepted.push(part.reduce((a,b) => a+b,0))
+        }
+    })
+return accepted.reduce((a,b) => a+b,0)
+}
+
+console.log(part1(inputData))
---
+++ aoc/day19/part2.js
@@ -0,0 +1,63 @@
+const { inputData, example } = require("./data.js");
+
+let [flowsLit, ] = inputData.split("\n\n"),
+    flows = {};
+
+flowsLit.split("\n").forEach(line => {
+    let [id, opsLit] = line.match(/[^({|})]+/g);
+    let ops = opsLit.split(',');
+    flows[id] = ops.map(opLit => {
+        let tmp = opLit.split(/:/g);
+        if (tmp.length == 1) return tmp;
+        let [k, v] = tmp[0].split(/<|>/g);
+        return [k, tmp[0][1], Number(v), tmp[1]];
+    });
+})
+
+let sum = 0, ents = Object.entries(flows);
+
+const backtrack = (resultId, b = {x: [1, 4000], m: [1, 4000], a: [1, 4000], s: [1, 4000]}) => ents.forEach(([id, flow]) => {
+    for (let condId = 0; condId < flow.length; condId++) {
+        if (flow[condId][flow[condId].length-1] == resultId) {
+            let i = condId,
+                bounds = {x: b.x.slice(), m: b.m.slice(), a: b.a.slice(), s: b.s.slice()}
+
+            while (i >= 0) {
+                if (flow[i].length == 1) {i--; continue;}
+                let [par, op, val] = flow[i];
+                if (i == condId) {
+                    //flow[j] was satisfied
+                    if (op == '>') {
+                        // x > 5 was satisfied
+                        if (bounds[par][0] <= val) bounds[par][0] = val+1;
+                    } else {
+                        // x < 5 was satisfied
+                        if (bounds[par][1] >= val) bounds[par][1] = val-1;
+                    }
+                } else {
+                    //flow[j] was not satisfied
+                    if (op == '>') {
+                        // x > 5 was not satisfied
+                        if (bounds[par][1] > val) bounds[par][1] = val;
+                    } else {
+                        // x < 5 was not satisfied
+                        if (bounds[par][0] < val) bounds[par][0] = val;
+                    }
+                }
+                i--;
+            }
+
+            if (id == 'in') {
+                if (bounds.x[1] >= bounds.x[0] && bounds.m[1] >= bounds.m[0] && bounds.a[1] >= bounds.a[0] && bounds.s[1] >= bounds.s[0]) {
+                    sum += (bounds.x[1] - bounds.x[0]+1) * (bounds.m[1] - bounds.m[0]+1) * (bounds.a[1] - bounds.a[0]+1) * (bounds.s[1] - bounds.s[0]+1);
+                }
+            } else backtrack(id, bounds);
+        }
+    }
+})
+
+backtrack('A');
+
+console.log('p2', sum);
+
+
