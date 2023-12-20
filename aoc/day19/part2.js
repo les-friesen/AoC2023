ffd1affd4164760b15870224cc585ce34d10184f
“Lesley <you@example.com>
2023-12-20 00:15:42 -0500

    day 19 comments

--- aoc/day19/part2.js
+++ aoc/day19/part2.js
@@ -1,7 +1,6 @@
 const { inputData, example } = require("./data.js");
 
 // Not my original solution -->>  https://github.com/surgi1/adventofcode/blob/main/2023/day19/script.js
-// Copied here to learn from. 
 
 let [flowsLit, ] = inputData.split("\n\n"),
     flows = {};
